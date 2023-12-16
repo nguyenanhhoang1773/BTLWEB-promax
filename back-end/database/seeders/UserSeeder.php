fc8f9db134a064117e0e68334f7c4f23c0e9eaf6
dat hoang <datht.22ite@vku.udn.vn>
2023-12-16 09:14:48 +0700

    faker data

---
+++ back-end/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use \App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $faker = Factory::create();
+        // $group = new User();
+        // $group->name =$faker->name;
+        // $group->email =$faker->email;
+        // $group->password = Hash::make($faker->password);
+        // $group->save();
+
+        User::factory()->count(5)->create();
+    }
+}
