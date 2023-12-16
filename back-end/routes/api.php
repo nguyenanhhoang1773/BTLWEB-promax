2320116fbcce2482e1d60711676191a78bc9eeec
dat hoang <datht.22ite@vku.udn.vn>
2023-12-16 16:45:36 +0700

    middleware

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\ProductController;
+use App\Http\Controllers\User\UserController;
 
 
 /*
@@ -20,4 +21,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/login', [UserController::class, 'login'])->name('login');
+Route::post('/login', [UserController::class, 'postLogin']);
+Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::post('/register', [UserController::class, 'postRegister']);
+Route::get('/logoutAcc', [UserController::class, 'logoutAcc'])->name('logoutAcc');
+
+
 Route::get('/list-product',[ProductController::class,'getProduct']);
+Route::prefix('/admin')->middleware('admin')->group(function(){
+
+});
