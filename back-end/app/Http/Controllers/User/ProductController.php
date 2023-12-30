56551d11897385a3c2e6ec2b291687661a220a7d
dat hoang <datht.22ite@vku.udn.vn>
2023-12-30 17:12:32 +0700

    ok

--- back-end/app/Http/Controllers/User/ProductController.php
+++ back-end/app/Http/Controllers/User/ProductController.php
@@ -11,14 +11,20 @@ class ProductController extends Controller
 {
     public function getProduct()
     {
-        $product = Product::all();      
-        return $product;
+        $products = Product::all();
+
+        $productData = $products->map(function ($product) {
+            $product->image = asset('storage/images/' . $product->image);          
+            return $product;
+        });
+        // dd($productData);
+        return response()->json($productData);
     }
     public function getCategory()
     {
-        $category = Category::all();      
+        $category = Category::all();
         return $category;
-    } 
+    }
     public function insertData()
     {
 
@@ -51,7 +57,7 @@ class ProductController extends Controller
             'price'    => '3790000',
             'sale_price' => '3524700',
             'category_id' => '3',
-            'slug' =>'dan-guitar1',
+            'slug' => 'dan-guitar1',
             'description' => 'Yamaha FS100C với thiết kế đơn giản, tinh tế, sang và nhiều lựa chọn về màu sắc, đặc biệt dải âm thanh rất đẹp. 
             Chất lượng tốt, anh em chơi Guitar đánh giá tương đương với Yamaha F310 trong tầm giá. Đàn bảo hành 1 năm, bao đổi mới nếu có lỗi do nhà sản xuất.',
         ]);
@@ -62,7 +68,7 @@ class ProductController extends Controller
             'price'    => '5640000',
             'sale_price' => '5358000',
             'category_id' => '4',
-            'slug' =>'dan-guitar2',
+            'slug' => 'dan-guitar2',
             'description' => 'Nhỏ gọn và thoải mái, CC-60S LH lý tưởng cho người mới chơi và thuận tay trái. Đàn có dáng Concert nhỏ gọn, dễ dàng chơi ở bất kỳ vị trí nào với âm thanh tuyệt vời.
              Đầu spruce rắn tinh xảo, cổ dễ chơi, và mặt sau và mặt gỗ gụ làm cho CC-60S LH trở thành lựa chọn hoàn hảo để chơi ở bãi biển, sân vườn hoặc quán cà phê.',
         ]);
@@ -85,7 +91,7 @@ class ProductController extends Controller
             'price'    => '14340000',
             'sale_price' => '13623000',
             'category_id' => '6',
-            'slug' =>'dan-violin2',
+            'slug' => 'dan-violin2',
             'description' => 'Suzuki Violin NS 20FIT 3/4, Kiểu dáng, thiết kế đẹp và hài hòa, Thiết kế nổi bật, màu sắc sang trọng, Cạnh tranh về giá và cạnh tranh về chất lượng
             Kích thước sản phẩm (D x R x C cm)    90 x 25 x 10
             Trọng lượng (KG)    2.5',
@@ -97,7 +103,7 @@ class ProductController extends Controller
             'price'    => '26800000',
             'sale_price' => '25900000',
             'category_id' => '7',
-            'slug' =>'ken-saxophone1',
+            'slug' => 'ken-saxophone1',
             'description' => 'Kèn Tenor Saxophone Selmer TS651 phù hợp với với cả sinh viên lẫn nhà 
             giáo dục âm nhạc, từ nghiệp dư đến chuyên nghiệp, những người muốn chơi các dụng cụ Kèn Saxophone .',
         ]);
@@ -108,7 +114,7 @@ class ProductController extends Controller
             'price'    => '34200000',
             'sale_price' => '31000000',
             'category_id' => '8',
-            'slug' =>'ken-saxophone2',
+            'slug' => 'ken-saxophone2',
             'description' => 'Conn TS650 Student Tenor Saxophones mang đến cho người sử dụng một âm thanh chất lượng và một thiết kế khá tiện ích, nổi bật',
         ]);
 
@@ -118,7 +124,7 @@ class ProductController extends Controller
             'price'    => '20700000',
             'sale_price' => '19665000',
             'category_id' => '9',
-            'slug' =>'bo-trong1',
+            'slug' => 'bo-trong1',
             'description' => 'Với kinh nghiệm 70 năm trong nghề chế tác trống thủ công, Pearl DMP925SP/C là model đỉnh cao để bạn sẵn sàng học tập và biểu diễn. 
             Lớp vỏ Maple 5,4mm, các tính năng chuyên nghiệp và lớp hoàn thiện sơn mài tuyệt đẹp, Decade giúp nâng cao hiệu suất chơi trống của bạn tối đa với mức giá mà trước đây các model khác không thể đạt được.
              Phần cứng vỏ trống có khối lượng nhẹ / tiếp xúc thấp đảm bảo hiệu suất tối ưu và độ bền của cả bộ trống. ',
@@ -130,7 +136,7 @@ class ProductController extends Controller
             'price'    => '35570000',
             'sale_price' => '34070000',
             'category_id' => '10',
-            'slug' =>'bo-trong2',
+            'slug' => 'bo-trong2',
             'description' => 'Bộ trống Jazz Yamaha Rydeen Brown mới (combo 5 món) là sản phẩm dành cho cả người mới bắt đầu lẫn tay chơi trống chuyên nghiệp. 
             Màn ra mắt này đã tạo cơ hội trải nghiệm mới mẻ dành cho cộng đồng chơi trống. Mặc dù không có sự thay đổi mạnh mẽ ở phần ngoại hình, bộ trống lại được nâng cấp với phiên bản đầy tính đột phá ở phần hiệu năng.',
         ]);
