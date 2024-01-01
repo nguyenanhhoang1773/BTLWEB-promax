e8270acce95cfa6f61d013c352795967e17be52e
dat hoang <datht.22ite@vku.udn.vn>
2024-01-01 16:14:10 +0700

    ok

--- back-end/resources/views/admin/product/add.blade.php
+++ back-end/resources/views/admin/product/add.blade.php
@@ -144,18 +144,5 @@
                 console.error(error);
             });
     </script>
-    @php
-        function showCategories($categories, $parent_id = 0, $char = '')
-        {
-            foreach ($categories as $key => $item) {
-                // Nếu là chuyên mục con thì hiển thị
-                if ($item->parent_id == $parent_id) {
-                    echo '<option value="' . $item->id . '">' . $char . $item->name . '</option>';
-                    unset($categories[$key]);
-                    // Tiếp tục đệ quy để tìm chuyên mục con của chuyên mục đang lặp
-                    showCategories($categories, $item->id, $char . '--');
-                }
-            }
-        }
-    @endphp
+    
 @endsection
