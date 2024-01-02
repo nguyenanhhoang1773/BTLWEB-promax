f2e3b7af730dff385d16b463675b968ad78bddc8
nguyenanhhoang1773 <nguyenanhhoang170703@gmail.com>
2024-01-02 19:31:33 +0700

    đa

--- front-end/src/Components/cartItem/index.jsx
+++ front-end/src/Components/cartItem/index.jsx
@@ -1,11 +1,26 @@
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useDispatch } from "react-redux";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 import { removeProduct } from "../../features/CartManage/CartSlice";
 import "./main.css";
 function CartItem({ id, name, image, price, sale_price }) {
-  console.log(id, name, price, image, sale_price);
+  const idUser = useSelector((state) => state.login.id);
   const dispatch = useDispatch();
+  const handleRemovePd = () => {
+    axios
+      .post("http://localhost:8000/api/deletecart", {
+        customerid: idUser,
+        productid: id,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    dispatch(removeProduct(id));
+  };
   return (
     <div className="flex mt-[12px] opacity-90  border-l-[2px]  border-r-[2px]  border-yellow-500 bg-white rounded-md">
       <img className="w-[160px] pl-[10px] py-[4px] rounded-md" src={image} />
@@ -24,9 +39,7 @@ function CartItem({ id, name, image, price, sale_price }) {
       <div className="flex-1 flex justify-end pr-[30px] items-center">
         <FontAwesomeIcon
           className="text-yellow-500 icon hover:cursor-pointer hover:text-yellow-400 text-[40px]"
-          onClick={() => {
-            dispatch(removeProduct(id));
-          }}
+          onClick={handleRemovePd}
           icon={faTrash}
         />
       </div>
