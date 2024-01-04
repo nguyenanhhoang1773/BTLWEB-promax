cf51b6003719960030bebb523d9d31e8568ee5c0
nguyenanhhoang1773 <nguyenanhhoang170703@gmail.com>
2024-01-05 01:32:41 +0700

    adad

--- front-end/src/Pages/ListPage.jsx
+++ front-end/src/Pages/ListPage.jsx
@@ -6,7 +6,8 @@ import { useParams } from "react-router-dom";
 import Item from "../Components/Item";
 function ListPage() {
   const { type: type } = useParams();
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const allPd = useSelector((state) => state.storeProducts.all);
   useEffect(() => {
     if (type === "all") {
@@ -23,30 +24,47 @@ function ListPage() {
         setProducts(response.data);
       })
       .catch(function (error) {
+        setProducts([]);
         console.log(error);
       })
       .finally(function () {
         // always executed
       });
+    setTimeout(() => {
+      setLoading(true);
+    }, 500);
+    console.log(products);
   }, [type]);
   return (
     <>
-      {products && (
-        <div className="flex flex-wrap">
-          {products.map(
-            ({ id, slug, name, price, image, sale_price }, index) => (
-              <Item
-                list
-                key={index}
-                slug={slug}
-                id={id}
-                name={name}
-                image={image}
-                price={price}
-                sale_price={sale_price}
-              />
-            )
-          )}
+      {products.length > 0 && loading && (
+        <>
+          <h3 className="text-white ml-[10px] font-[600] text-[22px]">
+            Kết quả tìm kiếm cho "{type}".
+          </h3>
+          <div className="flex flex-wrap">
+            {products.map(
+              ({ id, slug, name, price, image, sale_price }, index) => (
+                <Item
+                  list
+                  key={index}
+                  slug={slug}
+                  id={id}
+                  name={name}
+                  image={image}
+                  price={price}
+                  sale_price={sale_price}
+                />
+              )
+            )}
+          </div>
+        </>
+      )}
+      {products.length <= 0 && (
+        <div className="min-h-[600px]">
+          <h3 className="text-white ml-[10px] font-[600] text-[22px]">
+            Không có sản phẩm nào được tìm thấy với từ khóa "{type}".
+          </h3>
         </div>
       )}
     </>
