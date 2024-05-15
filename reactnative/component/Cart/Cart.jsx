baf4b14222d5aa4d5dc28b04266878aeffc8b12d
unknown <nguyenanhhoang170703@gmail.com>
2024-05-16 00:27:40 +0700

    test-commit

--- reactnative/component/Cart/Cart.jsx
+++ reactnative/component/Cart/Cart.jsx
@@ -1,58 +1,59 @@
-import {
-  FlatList,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ItemCart from "../ItemCart/ItemCart";
 const data = [
   {
     title: "Áo khoác",
+    branch: "luxury",
+
     price: 310,
+
     url: "https://dosi-in.com/file/detailed/472/dosiin-highclub-ao-hoodie-basic-mu-trum-do-ni-cotton-highclub-brand-chinh-hang-472344472344.jpg?w=670&h=670&fit=fill&fm=webp",
   },
   {
     title: "Quần đùi",
+    branch: "luxury",
     price: 190,
     url: "https://dosi-in.com/file/detailed/472/dosiin-highclub-ao-hoodie-basic-mu-trum-do-ni-cotton-highclub-brand-chinh-hang-472344472344.jpg?w=670&h=670&fit=fill&fm=webp",
   },
   {
     title: "jean",
+    branch: "luxury",
     price: 310,
     url: "https://dosi-in.com/file/detailed/472/dosiin-highclub-ao-hoodie-basic-mu-trum-do-ni-cotton-highclub-brand-chinh-hang-472344472344.jpg?w=670&h=670&fit=fill&fm=webp",
   },
   {
     title: "Bomber",
+    branch: "luxury",
     price: 420,
     url: "https://dosi-in.com/file/detailed/472/dosiin-highclub-ao-hoodie-basic-mu-trum-do-ni-cotton-highclub-brand-chinh-hang-472344472344.jpg?w=670&h=670&fit=fill&fm=webp",
   },
   {
     title: "Túi xách",
+    branch: "luxury",
     price: 800,
     url: "https://dosi-in.com/file/detailed/472/dosiin-highclub-ao-hoodie-basic-mu-trum-do-ni-cotton-highclub-brand-chinh-hang-472344472344.jpg?w=670&h=670&fit=fill&fm=webp",
   },
   {
     title: "Khẩu trang",
+    branch: "luxury",
     price: 5,
     url: "https://dosi-in.com/file/detailed/472/dosiin-highclub-ao-hoodie-basic-mu-trum-do-ni-cotton-highclub-brand-chinh-hang-472344472344.jpg?w=670&h=670&fit=fill&fm=webp",
   },
   {
     title: "Hoodie",
+    branch: "luxury",
     price: 440,
     url: "https://dosi-in.com/file/detailed/472/dosiin-highclub-ao-hoodie-basic-mu-trum-do-ni-cotton-highclub-brand-chinh-hang-472344472344.jpg?w=670&h=670&fit=fill&fm=webp",
   },
 ];
-const renderItem = ({
-  item,
-  index,
-}) => {
+const renderItem = ({ item, index }) => {
   return (
     <ItemCart
       title={item.title}
       price={item.price}
       url={item.url}
+      branch={item.branch}
     />
   );
 };
@@ -71,17 +72,10 @@ const Cart = () => {
         }}
       >
         <View>
-          <Text
-            style={styles.text_header}
-          >
-            Giỏ hàng
-          </Text>
+          <Text style={styles.text_header}>Giỏ hàng</Text>
         </View>
       </View>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-      />
+      <FlatList data={data} renderItem={renderItem} />
     </SafeAreaView>
   );
 };
--- reactnative/component/ItemCart/ItemCart.jsx
+++ reactnative/component/ItemCart/ItemCart.jsx
@@ -1,58 +1,171 @@
 import { useState } from "react";
 import {
   Image,
+  Pressable,
   StyleSheet,
   Switch,
   Text,
+  TouchableHighlight,
   View,
 } from "react-native";
 import CheckBox from "react-native-check-box";
 import { colors } from "../../constants/constants";
-function ItemCart({
-  title,
-  price,
-  url,
-}) {
-  const [isEnabled, setIsEnabled] =
-    useState(false);
+
+import { AntDesign } from "@expo/vector-icons";
+
+function ItemCart({ title, price, url, sale, branch }) {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [quantity, setQuantity] = useState(0);
+  const handleSelect = () => {};
+  const handleDelete = () => {
+    alert("deleted");
+  };
+  const increaseQuantity = () => {
+    setQuantity((pre) => ++pre);
+  };
+  const decreaseQuantity = () => {
+    if (quantity <= 0) {
+      return;
+    }
+    setQuantity((pre) => --pre);
+  };
+
   return (
-    <View style={styles.wrapper}>
-      <CheckBox
-        style={{
-          padding: 10,
-          borderRadius: 30,
-        }}
-        onClick={() =>
-          setIsEnabled((pre) => !pre)
-        }
-        isChecked={isEnabled}
-        // leftText={"CheckBox"}
-      />
-      <Image
-        style={styles.img}
-        source={{
-          uri: url,
-        }}
-      />
-      <View style={styles.description}>
-        <Text style={styles.title}>
-          {title}
-        </Text>
-        <Text style={styles.price}>
-          {price}đ
+    <View>
+      <View style={styles.branch}>
+        <Text
+          style={{ fontSize: 22, paddingVertical: 6, paddingHorizontal: 26 }}
+        >
+          {branch}
         </Text>
       </View>
+      <TouchableHighlight
+        onPress={handleSelect}
+        underlayColor={colors.secondary}
+        style={styles.touchable}
+      >
+        <View style={styles.wrapper}>
+          <View
+            style={{
+              flexDirection: "row",
+            }}
+          >
+            <Pressable
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                marginRight: 4,
+              }}
+              onPress={handleDelete}
+            >
+              <Text
+                style={{
+                  color: colors.primary,
+                  fontSize: 16,
+                  padding: 4,
+                  marginRight: 8,
+                }}
+              >
+                Xoá
+              </Text>
+            </Pressable>
+            <Image
+              style={styles.img}
+              source={{
+                uri: url,
+              }}
+            />
+            <View style={styles.description}>
+              <Text style={styles.title}>{title}</Text>
+              <Text style={styles.price}>{price}đ</Text>
+            </View>
+          </View>
+          <View style={styles.counter}>
+            <TouchableHighlight
+              underlayColor={colors.secondary}
+              style={{
+                borderWidth: 1,
+                borderColor: colors.tertiary,
+                width: 24,
+                height: 24,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={decreaseQuantity}
+            >
+              <Text style={{ color: colors.tertiary }}>-</Text>
+            </TouchableHighlight>
+            <View
+              style={{
+                borderWidth: 1,
+                borderColor: colors.tertiary,
+                width: 24,
+                height: 24,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={{ color: colors.tertiary }}>{quantity}</Text>
+            </View>
+            <TouchableHighlight
+              underlayColor={colors.secondary}
+              style={{
+                borderWidth: 1,
+                borderColor: colors.tertiary,
+                width: 24,
+                height: 24,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={increaseQuantity}
+            >
+              <Text style={{ color: colors.tertiary }}>+</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <View
+          style={{
+            borderTopWidth: 1,
+            borderColor: "#ccc",
+            alignItems: "center",
+            padding: 4,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 16,
+              color: colors.primary,
+            }}
+          >
+            Voucher giảm đến {sale}k
+          </Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 }
 
 export default ItemCart;
 const styles = StyleSheet.create({
+  color: {
+    color: "#aaa",
+  },
+  branch: {
+    borderTopWidth: 8,
+    borderColor: colors.secondary,
+  },
+  touchable: {
+    borderTopWidth: 1,
+    borderColor: "#ccc",
+  },
   wrapper: {
     flexDirection: "row",
+    justifyContent: "space-between",
     flex: 1,
-    backgroundColor: "white",
-    padding: 16,
+    paddingVertical: 20,
+    paddingHorizontal: 24,
     // alignItems: "center",
   },
   img: {
@@ -68,5 +181,9 @@ const styles = StyleSheet.create({
   price: {
     color: colors.primary,
     fontSize: 16,
+  },
+  counter: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
